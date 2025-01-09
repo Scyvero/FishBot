978e1379b0878b4036562d95e192114e8e03b897
Scyvero <silthegamerguy@gmail.com>
2025-01-09 15:54:58 +0100

    fixed soem stuff

---
+++ Commands/prefixes/testCommand.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'test', // Command name
+    async execute(message, args) {
+        // Join the remaining arguments back into a string
+        const textContent = args.join(' ');
+        await message.reply(`You said: "${textContent}"`);
+    },
+};
