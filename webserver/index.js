df6d28a1c759ab82cb97a411bea9c3efd5cfad5a
Scyvero <silthegamerguy@gmail.com>
2025-01-24 10:52:50 +0100

    put everyting into a subfolder
    everything into a subfolder, also made a webserver

---
+++ webserver/index.js
@@ -0,0 +1,54 @@
+const { request } = require('undici');
+const express = require('express');
+const path = require('path');
+const { port } = require('./config.json');
+
+const app = express();
+
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', async ({ query }, response) => {
+	const { code } = query;
+
+	if (code) {
+		try {
+			const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientId,
+					client_secret: clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://localhost:${port}`,
+					scope: 'identify',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+			const oauthData = await tokenResponseData.body.json();
+			console.log(oauthData);
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error
+			// tokenResponseData.statusCode will be 401
+			console.error(error);
+		}
+	}
+
+	return response.sendFile('index.html', { root: '.' });
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+}).on('error', (err) => {
+    console.error('Server failed to start:', err);
+});
